630fbfcf7fd8feb4861757da2c12e9a38f17dceb
shichaohu <502242999@qq.com>
2020-11-11 14:32:06 +0800

    初始代码

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+using System;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "gRPC Request" });
+            Console.WriteLine("Greeter 服务返回数据: " + reply.Message);
+            Console.ReadKey();
+        }
+    }
+}
